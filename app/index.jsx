ed6def0d257a0678f9e46b3ebb8f97ec027b490a
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-11-12 21:04:02 -0300

    comment: remove index redirect

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,11 @@
 import { InitScreen } from "../src/screens/InitScreen";
+import { Redirect } from "expo-router";
 
 export default function index() {
+  // Para que no me traiga a index cuando estoy trabajando en home
+  // return <Redirect href={`/account`} />;
+  // return <Redirect href={`/signup`} />;
+  // return <Redirect href={`/raffleDetail/${"id-del-sorteo"}`} />;
+
   return <InitScreen />;
 }
