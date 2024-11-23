2cab4ffc8992bbc1f574f984af6bcffdd3681f70
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-11-23 20:08:44 -0300

    styles: RunRaffle

--- src/components/RunRaffleModal.jsx
+++ src/components/RunRaffleModal.jsx
@@ -5,10 +5,13 @@ import {
   Modal,
   StyleSheet,
   TouchableOpacity,
-  LogBox,
+  ImageBackground,
 } from "react-native";
 import { CheckBox } from "@rneui/themed";
 import { updateRaffleRealized } from "../utils/raffle_functions";
+import { NormalText } from "../ui/Texts";
+import { SmallRedButton, SmallYellowButtonWithDesabled } from "../ui/Buttons";
+import background from "../../assets/app/background_modal.png";
 
 export function RunRaffleModal({
   visibleState,
@@ -20,11 +23,8 @@ export function RunRaffleModal({
   const [check, setCheck] = useState(false);
 
   const handleRaffleRealized = async () => {
-    console.log("updating results...");
     const response = await updateRaffleRealized(raffleId, raffleResult);
     onUpdateComplete();
-    console.log("🚀 ~ handleRaffleRealized ~ response:", response);
-    console.log("resultados actualizados exitosamente");
   };
 
   return (
@@ -34,64 +34,67 @@ export function RunRaffleModal({
       visible={visibleState}
       onRequestClose={() => setVisibleState(false)} // En Android, para cerrar con el botón de atrás
     >
-      <View style={styles.modalBackground}>
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalText}>REALIZAR SORTEO</Text>
-          <Text style={styles.modalText}>
-            ¿ESTAS SEGURO QUE QUIERES REALIZAR EL SORTEO CON EL NOMBRE{" "}
-            {"NOMBRE SORTEO"}?
-          </Text>
-
-          <Text>
-            Esta accion no se puede deshacer, los resultados no podran
-            modificarse una vez realizados los cambios
-          </Text>
-
-          <View
-            style={{
-              flexDirection: "row",
-              alignContent: "center",
-              justifyContent: "center",
-            }}
-          >
-            <CheckBox
-              center
-              checked={check}
-              onPress={() => setCheck((prev) => !prev)}
-              iconType="material-community"
-              checkedIcon="checkbox-marked"
-              uncheckedIcon="checkbox-blank-outline"
-              checkedColor="purple"
+      <View style={[styles.overlay]}>
+        <ImageBackground
+          source={background}
+          style={{
+            backgroundColor: "red",
+            width: "100%",
+            height: 600,
+            paddingVertical: 30,
+          }}
+        >
+          <View style={{ gap: 45 }}>
+            <NormalText content={"REALIZAR SORTEO"} />
+            <NormalText
+              content={`¿ESTAS SEGURO QUE QUIERES REALIZAR EL SORTEO ${"NOMBRE SORTEO"}?`}
             />
-            <Text style={styles.label}>
-              Confirmo que quiero realizar el sorteo
-            </Text>
-          </View>
+            <NormalText
+              content={`Esta acción no puede revertirse y el sorteo se dara por cerrado una vez generado los resultados.`}
+            />
 
-          <View
-            style={{
-              flexDirection: "row",
-              gap: 20,
-              marginHorizontal: "auto",
-            }}
-          >
-            <TouchableOpacity
-              style={{ borderWidth: 1, borderColor: "red", padding: 10 }}
-              onPress={() => {
-                setVisibleState(false);
+            <View
+              style={{
+                flexDirection: "row",
+                alignContent: "center",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              <Text>Cancelar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{ borderWidth: 1, borderColor: "red", padding: 10 }}
-              disabled={!check}
-              onPress={handleRaffleRealized}
+              <CheckBox
+                center
+                checked={check}
+                onPress={() => setCheck((prev) => !prev)}
+                iconType="material-community"
+                checkedIcon="checkbox-marked"
+                uncheckedIcon="checkbox-blank-outline"
+                checkedColor="purple"
+              />
+              <NormalText content={"Confirmar"} />
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 20,
+                marginHorizontal: "auto",
+              }}
             >
-              <Text>Sortear</Text>
-            </TouchableOpacity>
+              <SmallRedButton
+                content={"Cancelar"}
+                onPressFunction={() => {
+                  setVisibleState(false);
+                }}
+              />
+
+              <SmallYellowButtonWithDesabled
+                content={"Sortear"}
+                onPressFunction={handleRaffleRealized}
+                disabled={!check}
+              />
+            </View>
           </View>
-        </View>
+        </ImageBackground>
       </View>
     </Modal>
   );
@@ -115,5 +118,81 @@ const styles = StyleSheet.create({
   },
   modalText: {
     marginHorizontal: "auto",
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Overlay semi-transparente
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  modalContent: {
+    width: "90%",
+    maxWidth: 400,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  inactiveModalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "90%",
+    maxWidth: 400,
+  },
+  backgroundImage: {
+    width: "100%",
+    paddingVertical: 40,
+    alignItems: "center",
+  },
+  inputContainer: {
+    gap: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    width: "90%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    gap: 10,
+    marginTop: 20,
+  },
+  removeText: {
+    color: "red",
+    marginTop: 10,
+  },
+  ticketNumber: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  propietaryText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  noteLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  noteText: {
+    fontSize: 14,
+  },
+  freeTicketText: {
+    fontSize: 16,
+    fontStyle: "italic",
+  },
+  closeButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  closeButtonText: {
+    fontSize: 16,
   },
 });
