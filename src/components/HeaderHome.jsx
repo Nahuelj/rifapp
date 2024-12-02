abc36d96a8ba236ae8e61334e0d712e41f130103
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-12-02 17:46:28 -0300

    feat: new /home

--- src/components/HeaderHome.jsx
+++ src/components/HeaderHome.jsx
@@ -1,23 +1,7 @@
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
-import { Link, usePathname } from "expo-router";
-import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
 import { useFonts } from "expo-font";
 
 export function HeaderHome() {
-  const [home, setHome] = useState(true);
-  const [account, setAccount] = useState(false);
-  const path = usePathname();
-
-  useEffect(() => {
-    if (path == "/home") {
-      setHome(true);
-      setAccount(false);
-    } else {
-      setHome(false);
-      setAccount(true);
-    }
-  }, [path]);
-
   const [fontsLoaded] = useFonts({
     "Poppins-SemiBold": require("../../assets/fonts/Poppins/Poppins-SemiBold.ttf"),
   });
@@ -41,29 +25,26 @@ export function HeaderHome() {
         width: 345,
       }}
     >
-      <Link asChild href="/home">
-        <TouchableOpacity style={{ flex: 1, justifyContent: "center" }}>
-          <Text
-            style={[
-              {
-                fontFamily: "Poppins-SemiBold",
-                paddingHorizontal: 10,
-                backgroundColor: "#FFC600",
-                textAlign: "center",
-                fontSize: 18,
-                flex: 1,
-                textAlignVertical: "center",
-                borderRadius: 5,
-                lineHeight: 46,
-                color: "#49108B",
-              },
-              !home && { opacity: 0.6 },
-            ]}
-          >
-            🎁 MIS SORTEOS 💖
-          </Text>
-        </TouchableOpacity>
-      </Link>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Text
+          style={[
+            {
+              fontFamily: "Poppins-SemiBold",
+              paddingHorizontal: 10,
+              backgroundColor: "#FFC600",
+              textAlign: "center",
+              fontSize: 18,
+              flex: 1,
+              textAlignVertical: "center",
+              borderRadius: 5,
+              lineHeight: 46,
+              color: "#49108B",
+            },
+          ]}
+        >
+          🎁 MIS SORTEOS 💖
+        </Text>
+      </View>
     </View>
   );
 }
